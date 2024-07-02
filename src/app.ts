8c0e4a52b1fa1ed22a685e1a2f43005379e0c904
eltatataa <davidtowo75@gmail.com>
2024-07-02 01:20:02 -0500

    startup rag

---
+++ src/app.ts
@@ -0,0 +1,64 @@
+import "dotenv/config";
+
+import { HuggingFaceInference } from "@langchain/community/llms/hf";
+import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
+
+import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { MemoryVectorStore } from "langchain/vectorstores/memory";
+import { pull } from "langchain/hub";
+import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+
+import { formatDocumentsAsString } from "langchain/util/document";
+import {
+  RunnableSequence,
+  RunnablePassthrough,
+} from "@langchain/core/runnables";
+
+(async () => {
+  const llm = new HuggingFaceInference({
+    model: "meta-llama/Meta-Llama-3-8B-Instruct",
+    // model: "mistralai/Mistral-7B-Instruct-v0.2",
+    // model: "OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5",
+  });
+  const embeddings = new HuggingFaceInferenceEmbeddings();
+
+  const loader = new CheerioWebBaseLoader(
+    "https://js.langchain.com/v0.2/docs/introduction/"
+  );
+
+  console.log("Loading documents...");
+  const docs = await loader.load();
+
+  console.log("Splitting documents...");
+  const textSplitter = new RecursiveCharacterTextSplitter({
+    chunkSize: 1000,
+    chunkOverlap: 200,
+  });
+  const splits = await textSplitter.splitDocuments(docs);
+
+  console.log("Generating embeddings...");
+  const vectorStore = await MemoryVectorStore.fromDocuments(
+    splits,
+    embeddings
+  );
+
+  // Retrieve and generate using the relevant snippets of the blog.
+  const retriever = vectorStore.asRetriever();
+  const prompt = await pull<ChatPromptTemplate>("rlm/rag-prompt");
+
+  const declarativeRagChain = RunnableSequence.from([
+    {
+      context: retriever.pipe(formatDocumentsAsString),
+      question: new RunnablePassthrough(),
+    },
+    prompt,
+    llm,
+    new StringOutputParser(),
+  ]);
+
+  console.log("Asking question...");
+  const res = await declarativeRagChain.invoke("What is langchain?");
+  console.log(res);
+})();
