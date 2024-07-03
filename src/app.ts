94c4a24d80bc057aaaf53b9135bc94350cd653f0
eltatataa <davidtowo75@gmail.com>
2024-07-03 00:42:36 -0500

    conversational functionality

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/
 import { CheerioWebBaseLoader } from "@langchain/community/document_loaders/web/cheerio";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { MemoryVectorStore } from "langchain/vectorstores/memory";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
+import { ChatPromptTemplate, MessagesPlaceholder } from "@langchain/core/prompts";
 import { StringOutputParser } from "@langchain/core/output_parsers";
 
 import { formatDocumentsAsString } from "langchain/util/document";
@@ -17,14 +17,14 @@ import {
 
 (async () => {
   const llm = new HuggingFaceInference({
-    model: "meta-llama/Meta-Llama-3-8B-Instruct",
-    // model: "mistralai/Mistral-7B-Instruct-v0.2",
+    // model: "meta-llama/Meta-Llama-3-8B-Instruct",
+    model: "mistralai/Mistral-7B-Instruct-v0.2",
     // model: "OpenAssistant/oasst-sft-4-pythia-12b-epoch-3.5",
   });
   const embeddings = new HuggingFaceInferenceEmbeddings();
 
   const loader = new CheerioWebBaseLoader(
-    "https://js.langchain.com/v0.2/docs/introduction/"
+    "https://js.langchain.com/v0.2/docs/tutorials/rag"
   );
 
   console.log("Loading documents...");
@@ -45,30 +45,69 @@ import {
 
   // Retrieve and generate using the relevant snippets of the blog.
   const retriever = vectorStore.asRetriever();
-  // custom RAG prompt
-  const template = `Use the following pieces of context to answer the question at the end.
-  If you don't know the answer, just say that you don't know, don't try to make up an answer.
-  Use three sentences maximum and keep the answer as concise as possible.
-  Always say "thanks for asking!" at the end of the answer.
 
-  {context}
+  const contextualizeQSystemPrompt = `Given a chat history and the latest user question
+  which might reference context in the chat history, formulate a standalone question
+  which can be understood without the chat history. Do NOT answer the question,
+  just reformulate it if needed and otherwise return it as is.`;
 
-  Question: {question}
+  const contextualizeQPrompt = ChatPromptTemplate.fromMessages([
+    ["system", contextualizeQSystemPrompt],
+    new MessagesPlaceholder("chat_history"),
+    ["human", "{question}"],
+  ]);
+  const contextualizeQChain = contextualizeQPrompt
+    .pipe(llm)
+    .pipe(new StringOutputParser());
 
-  Helpful Answer:`;
-  const prompt = ChatPromptTemplate.fromTemplate(template)
 
-  const declarativeRagChain = RunnableSequence.from([
-    {
-      context: retriever.pipe(formatDocumentsAsString),
-      question: new RunnablePassthrough(),
-    },
-    prompt,
-    llm,
-    new StringOutputParser(),
+  const qaSystemPrompt = `You are an assistant for question-answering tasks.
+  Use the following pieces of retrieved context to answer the question.
+  If you don't know the answer, just say that you don't know.
+  Use three sentences maximum and keep the answer concise.
+
+  {context}`;
+
+  const qaPrompt = ChatPromptTemplate.fromMessages([
+    ["system", qaSystemPrompt],
+    new MessagesPlaceholder("chat_history"),
+    ["human", "{question}"],
   ]);
 
-  console.log("Asking question...");
-  const res = await declarativeRagChain.invoke("What is langchain?");
-  console.log(res);
+  const contextualizedQuestion = (input: Record<string, unknown>) => {
+    if ("chat_history" in input) {
+      return contextualizeQChain;
+    }
+    return input.question;
+  };
+
+  const ragChain = RunnableSequence.from([
+    RunnablePassthrough.assign({
+      context: (input: Record<string, unknown>) => {
+        if ("chat_history" in input) {
+          const chain: any = contextualizedQuestion(input);
+          return chain.pipe(retriever).pipe(formatDocumentsAsString);
+        }
+        return "";
+      },
+    }),
+    qaPrompt,
+    llm,
+  ]);
+
+  let chat_history: string[]  = [];
+
+  const firstQuestion = "What is a RAG?";
+  const response1 = await ragChain.invoke({ question: firstQuestion, chat_history });
+  console.log(response1);
+  chat_history = chat_history.concat(response1);
+
+  const secondQuestion = "How can i make one?";
+  const response2 = await ragChain.invoke({ question: secondQuestion, chat_history });
+  console.log(response2);
+  chat_history = chat_history.concat(response2);
+
+  const thirdQuestion = "And what concepts should you have to build one?";
+  const response3 = await ragChain.invoke({ question: thirdQuestion, chat_history });
+  console.log(response3);
 })();
